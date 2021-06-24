639c4a215aafde0aa5829589fc93ffe0599e678b
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2021-06-24 22:02:30 +0700

    update index modules

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,16 @@
-const boutiqueMap = require('./boutiques/endpointMap')
-const testMap = require('./test/endpointMap')
+const fs = require('fs');
+const path = require('path');
+const basename = path.basename(__filename);
+let endpointMap = {};
 
-module.exports = {
-  ...boutiqueMap,
-  ...testMap
-}
+fs
+  .readdirSync(__dirname)
+  .filter(dir => {
+    return (dir.indexOf('.') !== 0) && (dir !== basename) && (dir !== "dummy");
+  })
+  .forEach(dir => {
+    const dirEndpointMap = require(path.join(__dirname, dir, "endpointMap.js"));
+    endpointMap = { ...endpointMap, ...dirEndpointMap };
+  });
+
+module.exports = endpointMap;
