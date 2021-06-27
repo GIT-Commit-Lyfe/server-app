3658646f763e8de83afb405c8f055f2221920951
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2021-06-27 21:34:47 +0700

    update config

--- config/config.js
+++ config/config.js
@@ -1,28 +1,16 @@
 const { PGUSERNAME, PGPASSWORD, PGDB, PGHOST, PGPORT } = process.env;
 
+const config = {
+  username: PGUSERNAME,
+  password: PGPASSWORD,
+  database: PGDB,
+  host: PGHOST,
+  port: PGPORT,
+  dialect: "postgres"
+}
+
 module.exports = {
-  development: {
-    username: PGUSERNAME,
-    password: PGPASSWORD,
-    database: PGDB,
-    host: PGHOST,
-    port: PGPORT,
-    dialect: "postgres"
-  },
-  test: {
-    username: PGUSERNAME,
-    password: PGPASSWORD,
-    database: PGDB,
-    host: PGHOST,
-    port: PGPORT,
-    dialect: "postgres"
-  },
-  production: {
-    username: PGUSERNAME,
-    password: PGPASSWORD,
-    database: PGDB,
-    host: PGHOST,
-    port: PGPORT,
-    dialect: "postgres"
-  }
+  development: config,
+  test: config,
+  production: config
 }
