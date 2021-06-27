4cb56e9338cccf3c3566ae02eae690edd177c412
Muhammad Yusuf <muhammad_yusuf22@outlook.co.id>
2021-06-27 23:44:14 +0700

    add distance calculator
    hope this is correct

---
+++ helpers/distance-calculator.js
@@ -0,0 +1,19 @@
+function closestLocation(targetLocation, locationData) {
+  function vectorDistance(dx, dy) {
+    return Math.sqrt(dx * dx + dy * dy)
+  }
+
+  function locationDistance(location1, location2) {
+    let dx = location1.latitude - location2.latitude
+    let dy = location1.longitude - location2.longitude
+    return vectorDistance(dx, dy)
+  }
+
+  return locationData.reduce((prev, curr) => {
+    let prevDistance = locationDistance(targetLocation, prev)
+    let currDistance = locationDistance(targetLocation, curr)
+    return (prevDistance < currDistance) ? prev : curr
+  }, {});
+}
+
+module.exports = closestLocation
