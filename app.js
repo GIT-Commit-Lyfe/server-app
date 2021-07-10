365306c73b4743407238b35ac54bd6e2d2273626
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2021-07-10 16:52:30 +0700

    edit auth endpoint

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 var apiRouter = require('./routes/api');
 var dummyRouter = require('./routes/dummy')
 
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 app.use("/api", apiRouter);
 app.use("/dummy", dummyRouter);
 
