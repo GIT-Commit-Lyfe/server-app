0668269c2df3e093f4969ebd1cc9b1cd92538fc1
Muhammad Ramadhoni <ramadhoni@keyreply.com>
2021-07-15 22:07:53 +0700

    create cms-login endpoint

--- modules/auth/endpointMap.js
+++ modules/auth/endpointMap.js
@@ -1,36 +1,68 @@
 const log = require("../../utils/log");
 const { cmsAuthorize } = require("./middleware");
+const { verifyPassword, generateToken } = require("./helpers");
 
 module.exports = {
   "cms-login": [
     cmsAuthorize,
-    (req, res, next) => {
-      log.info("PONG");
-      res.send("PONG");
+    async (req, res, next) => {
+      if (req.method !== "POST") {
+        const message = "invalid method";
+        log.warn(message);
+        res.status(405).json({ message });
+        return;
+      }
+
+      const { username, password } = req.body;
+      if (!username || !password) {
+        const message = "undefined credential";
+        log.warn(message);
+        res.status(400).json({ message });
+        return;
+      }
+
+      try {
+        const userFound = await findUserByUsername(username);
+        if (!userFound) {
+          const message = "invalid credential";
+          log.warn(message)
+          res.status(400).json({ message });
+          return;
+        }
+
+        const passwordVerified = verifyPassword(password, userFound.password);
+        if (!passwordVerified) {
+          const message = "invalid credential";
+          log.warn(message)
+          res.status(400).json({ message });
+          return;
+        }
+
+        const payload = {
+          email: userFound.email,
+          name: userFound.name,
+          username: userFound.username,
+        }
+
+        const token = generateToken(payload);
+        res.status(200).json({ token });
+      } catch(err) {
+        const message = "internal server error";
+        log.error(message);
+        log.error(err.message);
+        log.error(err);
+        res.status(400).json({ message });
+      }
     }
   ],
   "app-login": [
     (req, res, next) => {
-      log.info("message received!");
-
-      const { routeId } = req.params;
-      let outputText = "you tested too much.";
-      if (routeId) {
-        outputText += ` here is your params id => ${routeId}`;
-      }
-      res.send(outputText);
+      
     }
   ],
   "app-signup": [
     (req, res, next) => {
-      log.info("message received!");
-
-      const { routeId } = req.params;
-      let outputText = "you tested too much.";
-      if (routeId) {
-        outputText += ` here is your params id => ${routeId}`;
-      }
-      res.send(outputText);
+      
     }
   ],
 }
