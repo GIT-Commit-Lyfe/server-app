2fbb2fffe61c2fe547452ec847f987ffd00bf921
Dhoni <ramadhoni@keyreply.com>
2021-08-22 21:47:10 +0700

    update deleteUpload middleware

--- middleware/deleteUpload.js
+++ middleware/deleteUpload.js
@@ -2,7 +2,15 @@ const { deleteFile } = require("../helpers/g-cloud-storage");
 module.exports = async (req, res, next) => {
   const { url } = req.body;
   if (url) {
-    deleteFile(url);
+    if (Array.isArray(url)) {
+      const allString = url.every((item) => typeof item === "string");
+      if (allString) {
+        const promises = url.map(item => deleteFile(item));
+        Promise.all(promises);
+      }
+    } else if (typeof url === "string") {
+      deleteFile(url);
+    }
   }
   next();
 }
