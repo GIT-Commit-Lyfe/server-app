d2e4e5d6d0b42ef4bebb521cccfde3647fd3034f
Dhoni <ramadhoni@keyreply.com>
2021-10-24 22:25:14 +0700

    fixing dial migration name

--- migrations/20211024192200-create-dial.js
+++ migrations/20211024192200-create-dial.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Bracelets', {
+    await queryInterface.createTable('Dials', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -37,6 +37,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Bracelets');
+    await queryInterface.dropTable('Dials');
   }
 };
